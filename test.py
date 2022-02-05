cd87e78df99cbb4d191196904f647fedc7702763
kotahorii <khorii.micron@gmail.com>
2022-02-05 16:32:06 +0900

    [update] test.py

--- test.py
+++ test.py
@@ -2,7 +2,19 @@
 # for i in range(len(name)):
 #     print(name[i])
 
-word1: str = input("input1: ")
-word2: str = input("input2: ")
+# word1: str = input("input1: ")
+# word2: str = input("input2: ")
 
-print(f"私は今日{word1}を書いて、{word2}に送った")
+# print(f"私は今日{word1}を書いて、{word2}に送った")
+
+# literal: str = "aldous Huxley was born in 1894".capitalize()
+# print(literal)
+
+# word = "どこで？ だれが？ いつ？"
+# print(word.split(" "))
+
+# new_list: list[str] = ["The", "fox", "jumped", "over", "the", "fence", "."]
+# print(" ".join(new_list).replace(" .", "."))
+
+literal: str = "A screaming comes across the sky."
+print(literal.replace("s", "$"))
