a88e290a50da093697c2705090cf31c59a341c26
kotahorii <khorii.micron@gmail.com>
2022-02-05 14:47:03 +0900

    [update] test.py

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-from typing import Union
+# from typing import Union
 
 
-def convert_to_float(x: Union[int, float]):
-    try:
-        print(float(x))
-    except ValueError:
-        print("x is not valid")
+# def convert_to_float(x: Union[int, float]):
+#     try:
+#         print(float(x))
+#     except ValueError:
+#         print("x is not valid")
 
 
-convert_to_float(3)
+# convert_to_float(3)
+
+# ListType = list[Union[int, str, bool]]
+
+# random: ListType = []
+# random.append(True)
+# random.append("abc")
+# random.append(100)
