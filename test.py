46b5deeed5b371b55483eaf12e5da86fe5a9d973
kotahorii <khorii.micron@gmail.com>
2022-02-05 14:28:46 +0900

    [update] test.py

--- test.py
+++ test.py
@@ -1,52 +1,12 @@
-# def f(x: int, y: int):
-#     return x * y
+def f(x: int):
+    return x**2
 
 
-# result = f(2, 3)
-# print(result)
-
-# print(len("Python"))
-
-# age: str = input("Enter your age: ")
-# int_age = int(age)
-# if int_age < 21:
-#     print("You are young!")
-# else:
-#     print("You are old")
+print(f(2))
 
 
-# def even_odd(x: int):
-#     if x % 2 == 0:
-#         print("even")
-#     else:
-#         print("odd")
+def g(a: int, b: int, c: int = 10, d: int = 20, e: int = 30):
+    return a + b + c + d + e
 
 
-# even_odd(3)
-# even_odd(2)
-
-
-# def f(x: int = 2):
-#     return x**x
-
-
-# print(f())
-# print(f(4))
-
-
-# def add_it(x: int, y: int = 10):
-#     return x + y
-
-
-# result = add_it(2)
-# print(result)
-
-
-# try:
-#     a: str = input("type a number: ")
-#     b: str = input("type b number: ")
-#     int_a = int(a)
-#     int_b = int(b)
-#     print(int_a / int_b)
-# except (ZeroDivisionError, ValueError):
-#     print("Invalid input")
+print(g(5, 15))
