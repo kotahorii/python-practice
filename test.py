71210e320a806cb97167ddf10d261d225e7a7647
kotahorii <khorii.micron@gmail.com>
2022-02-05 15:19:07 +0900

    [update] test.py

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-books: dict[str, str] = {"Dracula": "Stoker", "1984": "Orwell", "The Trial": "Kafka"}
-del books["The Trial"]
+# books: dict[str, str] = {"Dracula": "Stoker", "1984": "Orwell", "The Trial": "Kafka"}
+# del books["The Trial"]
 
-print(books)
+# print(books)
+
+songs: dict[str, str] = {"1": "fun", "2": "blue", "3": "me", "4": "floor", "5": "live"}
+n: str = input("数字を入力してください: ")
+
+if n in songs:
+    song: str = songs[n]
+    print(song)
+else:
+    print("見つかりません")
