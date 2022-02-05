4e67c72d8d97873beaa9132f9fcb4b3ff60763f3
kotahorii <khorii.micron@gmail.com>
2022-02-05 16:34:48 +0900

    [update] test.py

--- test.py
+++ test.py
@@ -1,20 +1,2 @@
-# name: str = "カミュ"
-# for i in range(len(name)):
-#     print(name[i])
-
-# word1: str = input("input1: ")
-# word2: str = input("input2: ")
-
-# print(f"私は今日{word1}を書いて、{word2}に送った")
-
-# literal: str = "aldous Huxley was born in 1894".capitalize()
-# print(literal)
-
-# word = "どこで？ だれが？ いつ？"
-# print(word.split(" "))
-
-# new_list: list[str] = ["The", "fox", "jumped", "over", "the", "fence", "."]
-# print(" ".join(new_list).replace(" .", "."))
-
-literal: str = "A screaming comes across the sky."
-print(literal.replace("s", "$"))
+word = "４月の晴れた寒い日で、時計がどれも十三時を打っていた。"
+print(word.split("、")[0])
