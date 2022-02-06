7a30a77d1409cc8af1f8243eeb504c655b9b6522
kotahorii <khorii.micron@gmail.com>
2022-02-06 15:38:20 +0900

    [Update] test.py

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-lists: list[str] = ["ウォーキング・デッド", "アントラージュ", "ザ・ソプラノズ", "ヴァンパイア・ダイアリーズ"]
+# lists: list[str] = ["ウォーキング・デッド", "アントラージュ", "ザ・ソプラノズ", "ヴァンパイア・ダイアリーズ"]
 
-for i, list in enumerate(lists):
-    print(f"{i}: {list}")
+# for i, list in enumerate(lists):
+#     print(f"{i}: {list}")
+
+list1: list[int] = [8, 19, 148, 4]
+list2: list[int] = [9, 1, 33, 83]
+new_list: list[int] = []
+
+for i in list1:
+    for j in list2:
+        new_list.append(i * j)
+
+print(new_list)
