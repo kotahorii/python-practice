b2223b12aa71d4c21e4b029f43f7affad938a220
kotahorii <khorii.micron@gmail.com>
2022-02-08 22:10:22 +0900

    [Update] object.py

--- object3.py
+++ object3.py
@@ -13,3 +13,15 @@
 # square3: Square = Square()
 
 # print(Square.square_list)
+
+
+# class Square:
+#     def __init__(self, len: int) -> None:
+#         self.len: int = len
+
+#     def __repr__(self) -> str:
+#         return f"{self.len} by {self.len} by {self.len} by {self.len}"
+
+
+# square: Square = Square(29)
+# print(square)
