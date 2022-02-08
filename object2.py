8962bea10573a9abeec05ea4d7c53879a9033497
kotahorii <khorii.micron@gmail.com>
2022-02-08 21:23:36 +0900

    [Update] object.py

---
+++ object2.py
@@ -0,0 +1,27 @@
+# class PublicPrivateExample:
+#     def __init__(self) -> None:
+#         self.public = "safe"
+#         self._unsafe = "unsafe"
+
+#     def public_method(self) -> None:
+#         pass
+
+#     def _unsafe_method(self) -> None:
+#         pass
+
+
+class Shape:
+    def __init__(self, width: int, len: int) -> None:
+        self.width: int = width
+        self.len: int = len
+
+    def print_size(self):
+        print(f"{self.width} by {self.len}")
+
+
+class Square(Shape):
+    pass
+
+
+a_square: Square = Square(20, 20)
+a_square.print_size()
