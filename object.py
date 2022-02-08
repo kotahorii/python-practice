4fd289b9fd61b26b57a26e46f5d58a66f9ce1d0f
kotahorii <khorii.micron@gmail.com>
2022-02-08 21:05:12 +0900

    [Update] object.py

--- object.py
+++ object.py
@@ -1,35 +1,38 @@
-# class Orange:
-#     def __init__(self, weight: int, color: str) -> None:
-#         self.weight: int = weight
-#         self.color: str = color
-#         self.mold: int = 0
-#         print("Created!")
-
-#     def rot(self, days: int, temp: int):
-#         self.mold = days * temp
+from math import pi
 
 
-# orange: Orange = Orange(200, "orange")
-# print(orange.mold)
-# orange.rot(10, 37)
-# print(orange.mold)
+class Circle:
+    def __init__(self, radius: int) -> None:
+        self.radius: int = radius
+
+    def area(self) -> float:
+        return pi * self.radius**2
 
 
-# class Rectangle:
-#     def __init__(self, width: int, len: int):
-#         self.width: int = width
-#         self.len: int = len
-
-#     def area(self):
-#         return self.width * self.len
-
-#     def change_size(self, width: int, len: int):
-#         self.width: int = width
-#         self.len: int = len
+circle: Circle = Circle(10)
+print(circle.area())
 
 
-# rectangle: Rectangle = Rectangle(10, 20)
-# print(rectangle.area())
-# rectangle.change_size(20, 40)
-# print(rectangle.area())
+class Triangle:
+    def __init__(self, width: int, height: int) -> None:
+        self.width: int = width
+        self.height: int = height
 
+    def area(self) -> float:
+        return self.width * self.height / 2
+
+
+triangle: Triangle = Triangle(2, 3)
+print(triangle.area())
+
+
+class Hexagon:
+    def __init__(self, edge_size: int) -> None:
+        self.edge_size = edge_size
+
+    def calculate_permeter(self) -> float:
+        return self.edge_size * 6
+
+
+hexagon: Hexagon = Hexagon(3)
+print(hexagon.calculate_permeter())
