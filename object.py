038cc0b9c523b1e19a89c8ce9d4f02c20e1dd758
kotahorii <khorii.micron@gmail.com>
2022-02-08 20:18:44 +0900

    [Update] object.py

---
+++ object.py
@@ -0,0 +1,35 @@
+# class Orange:
+#     def __init__(self, weight: int, color: str) -> None:
+#         self.weight: int = weight
+#         self.color: str = color
+#         self.mold: int = 0
+#         print("Created!")
+
+#     def rot(self, days: int, temp: int):
+#         self.mold = days * temp
+
+
+# orange: Orange = Orange(200, "orange")
+# print(orange.mold)
+# orange.rot(10, 37)
+# print(orange.mold)
+
+
+# class Rectangle:
+#     def __init__(self, width: int, len: int):
+#         self.width: int = width
+#         self.len: int = len
+
+#     def area(self):
+#         return self.width * self.len
+
+#     def change_size(self, width: int, len: int):
+#         self.width: int = width
+#         self.len: int = len
+
+
+# rectangle: Rectangle = Rectangle(10, 20)
+# print(rectangle.area())
+# rectangle.change_size(20, 40)
+# print(rectangle.area())
+
