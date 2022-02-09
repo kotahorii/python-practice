0789403cb8400bb43a1a8e45b205dc98067d17eb
kotahorii <khorii.micron@gmail.com>
2022-02-09 17:15:18 +0900

    [Update] regular expression

---
+++ regular_expression.py
@@ -0,0 +1,24 @@
+import re
+
+# l: str = "Beautiful is better than ugly"
+# matches: list[str] = re.findall("beautiful", l, re.IGNORECASE)
+
+# print(matches)
+
+# zen = """
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+# """
+
+# m: list[str] = re.findall("^If", zen, re.MULTILINE)
+# print(m)
+
+# string: str = "Two too."
+# m: list[str] = re.findall("t[ow]o", string, re.IGNORECASE)
+# print(m)
+
+line: str = "123 hi 34 hello"
+m: list[str] = re.findall("\d", line, re.IGNORECASE)
+print(m)
