058ed08eaeab1ad37963b0cadf5ff4eb316cabee
kotahorii <khorii.micron@gmail.com>
2022-02-10 17:05:14 +0900

    [Update] test.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,16 +9,49 @@
 #         print(i)
 
 
-def ss(number_list: list[int], n: int) -> bool:
-    found: bool = False
-    for i in number_list:
-        if i == n:
-            found = True
-            break
+# def ss(number_list: list[int], n: int) -> bool:
+#     found: bool = False
+#     for i in number_list:
+#         if i == n:
+#             found = True
+#             break
 
-    return found
+#     return found
 
 
-numbers: range = range(0, 101)
-s1 = ss(list(numbers), 200)
-print(s1)
+# numbers: range = range(0, 101)
+# s1 = ss(list(numbers), 200)
+# print(s1)
+
+
+# def palindrome(word: str) -> bool:
+#     word = word.lower()
+#     return word[::-1] == word
+
+
+# print(palindrome("Mother"))
+# print(palindrome("Mom"))
+
+
+# def anagram(w1: str, w2: str) -> bool:
+#     w1 = w1.lower()
+#     w2 = w2.lower()
+#     return sorted(w1) == sorted(w2)
+
+
+# print(anagram("iceman", "cinema"))
+# print(anagram("leaf", "tree"))
+
+
+def count_characters(string: str) -> None:
+    count_dict: dict[str, int] = {}
+    for c in string:
+        if c in count_dict:
+            count_dict[c] += 1
+        else:
+            count_dict[c] = 1
+
+    print(count_dict)
+
+
+count_characters("Dynasty")
