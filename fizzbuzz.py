ebb9af0d54d2061716ce62b333b262c758d30f9e
kotahorii <khorii.micron@gmail.com>
2022-02-10 15:36:35 +0900

    [Update] test.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,24 @@
-for i in range(1, 101):
-    if i % 15 == 0:
-        print("FizzBuzz")
-    elif i % 5 == 0:
-        print("Buzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    else:
-        print(i)
+# for i in range(1, 101):
+#     if i % 15 == 0:
+#         print("FizzBuzz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     else:
+#         print(i)
+
+
+def ss(number_list: list[int], n: int) -> bool:
+    found: bool = False
+    for i in number_list:
+        if i == n:
+            found = True
+            break
+
+    return found
+
+
+numbers: range = range(0, 101)
+s1 = ss(list(numbers), 200)
+print(s1)
